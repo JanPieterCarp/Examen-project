bc6a9b6779d43c594ccc625d0b10fd56afd87ae3
jan-Pieter Ott <janpieter.ott@student.rocva.nl>
2023-06-26 14:06:28 +0200

    1ste commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Jan-Pieter Martin Ott">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Mijn blog</title>
+  </head>
+    <body>
+        <h1>Hello world</h1>
+    </body>
+</html>
