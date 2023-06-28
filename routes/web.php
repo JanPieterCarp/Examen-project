d3a768d27e6d84e1170ab517b2a3e5bcc7c5229b
jan-Pieter Ott <janpieter.ott@student.rocva.nl>
2023-06-28 10:49:36 +0200

    post+routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('post', function () {
+    return view('posts');
 });
