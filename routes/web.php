7f3d49f91f9033685636b5441236c5d0b66c10f1
jan-Pieter Ott <janpieter.ott@student.rocva.nl>
2023-07-06 12:15:15 +0200

    routing gache 20 min

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', function () {
     return view('posts');
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
+    if(! file_exists($path = __DIR__."/../resources/posts/{$slug}.html")){
         return redirect('/');
-        //abort("404");
-
     }
 
-    $post = file_get_contents($path);
+
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), function() use ($path){
+        return file_get_contents($path);
+    });
+
 
     return view('post', [
     'post' => $post]);
-});
+})->where('post','[A-z_\-]+');
