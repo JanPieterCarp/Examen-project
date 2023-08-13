284c0b7d8da7fc32fad35d1673a893075a23d32e
jan-Pieter Ott <janpieter.ott@student.rocva.nl>
2023-08-13 15:08:15 +0200

    #commit

--- database/migrations/2023_08_13_125300_create_posts_table.php
+++ database/migrations/2023_08_13_125300_create_posts_table.php
@@ -11,9 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("excerpt");
+            $table->text("body");
             $table->timestamps();
+            $table->timestamps("pubblished_at")->nullable();
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('posts');
     }
 };
