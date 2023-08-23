1ff9fac69dbd473c4112589b65758fd26f40c70d
jan-Pieter Ott <janpieter.ott@student.rocva.nl>
2023-08-23 18:29:20 +0200

    login+logout

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,14 @@ use App\Models\User;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\sessionController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show'])->where('posts','[A-z_\-]+');
 
-Route::get('register', [RegisterController::class, 'create']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [sessionController::class, 'destroy']);
+Route::post('login', [sessionController::class, 'create']);
